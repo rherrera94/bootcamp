e7c0491c85124df20222624d64681764ea354eec
rherrera94 <rafael.herrera@outlook.com.ar>
2022-06-26 14:29:32 -0300

    nuevo tweet, api

---
+++ week9/fake-twitter/components/NewTweet.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { useSession } from 'next-auth/react'
+
+export default function NewTweet() {
+  //el setContent lo que hara es asignarle un valor a content el cual por defecto sera ''
+  const [content, setContent] = useState('')
+  const { data: session } = useSession()
+
+  if (!session || !session.user) return null
+
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault() //hace que no se produzca la accion por defecto que seria recargar la pagina
+
+        if (!content) {
+          alert('No content')
+          return
+        }
+        //el fetch este lo que hara es mandar una peticion POST a /api/tweet
+        fetch('/api/tweet', {
+          body: JSON.stringify({
+            content,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+        })
+      }}
+    >
+      <div className='flex'>
+        <div className='flex-1 px-1 pt-2 mt-2 mr-1 ml-1'>
+          <textarea
+            className='border p-4 w-full text-lg font-medium bg-transparent outline-none color-primary '
+            rows={2}
+            cols={50}
+            placeholder="What's happening?"
+            name='content'
+            onChange={(e) => setContent(e.target.value)} //cuando el valor de la textarea cambia le establezco a content el valor del textarea
+          />
+        </div>
+      </div>
+
+      <div className='flex'>
+        <div className='flex-1 mb-5'>
+          <button className='border float-right px-8 py-2 mt-0 mr-2 font-bold rounded-full'>
+            Tweet
+          </button>
+        </div>
+      </div>
+    </form>
+  )
+}
---
+++ week9/fake-twitter/pages/home.js
@@ -0,0 +1,12 @@
+import { useSession } from 'next-auth/react'
+import NewTweet from 'components/NewTweet'
+
+export default function Home() {
+  const { data: session, status } = useSession()
+
+  if (status === 'loading') {
+    return <p>...</p>
+  }
+
+  return <div>{session ? <NewTweet /> : <p>You are not logged in 😞</p>}</div>
+}
